d45f04ce4a184a70cfea74d73b14f83b90981ae6
Tobit Vervat <tobit.vervat@gmail.com>
2026-08-28 20:32:10 +0200

    Add GUI screen navigation and current-user interfaces

--- mainwindow.h
+++ mainwindow.h
@@ -1,26 +1,50 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QPoint>
+
+#include "database.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
+namespace Ui { class WelcomeScreen; }
+namespace Ui { class LoginScreen; }
+namespace Ui { class SignUpScreen; }
 QT_END_NAMESPACE
 
 class QListWidgetItem;
+class QStackedWidget;
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(Database& database, QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
+    void showLoginScreen();
+    void showSignupScreen();
+    void showWelcomeScreen();
+    void onLoginSubmitted();
+    void onSignupSubmitted();
+    void onLogoutClicked();
     void onAddTaskClicked();
-    void onRemoveTaskClicked();
     void onTaskItemChanged(QListWidgetItem* item);
+    void onTaskListContextMenuRequested(const QPoint& position);
 
 private:
     Ui::MainWindow *ui;
+    Ui::WelcomeScreen *welcomeUi;
+    Ui::LoginScreen *loginUi;
+    Ui::SignUpScreen *signupUi;
+    QStackedWidget *screenStack;
+    Database& database;
+    userInfo currentUser;
+
+    void loadCurrentUser();
+    bool saveCurrentTasks();
+    void updateDashboardUser();
+    void updateGreeting();
     void addTaskToList(const QString& text, bool done = false);
 };
