8b87c5a2270da75b1eb4e285d3c5b6e4a148c66f
Tobit Vervat <tobit.vervat@gmail.com>
2026-09-03 21:28:19 +0200

    Added declarations for new pages and features

--- mainwindow.h
+++ mainwindow.h
@@ -4,22 +4,41 @@
 #include <QPoint>
 
 #include "database.h"
+#include "weather.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-namespace Ui { class WelcomeScreen; }
-namespace Ui { class LoginScreen; }
-namespace Ui { class SignUpScreen; }
+namespace Ui
+{
+    class MainWindow;
+}
+namespace Ui
+{
+    class WelcomeScreen;
+}
+namespace Ui
+{
+    class LoginScreen;
+}
+namespace Ui
+{
+    class SignUpScreen;
+}
 QT_END_NAMESPACE
 
 class QListWidgetItem;
 class QStackedWidget;
+class QListWidget;
+class QNetworkAccessManager;
+class QTimer;
+class QLabel;
+class QLineEdit;
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow
+{
     Q_OBJECT
 
 public:
-    explicit MainWindow(Database& database, QWidget *parent = nullptr);
+    explicit MainWindow(Database &database, QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
@@ -30,8 +49,9 @@ private slots:
     void onSignupSubmitted();
     void onLogoutClicked();
     void onAddTaskClicked();
-    void onTaskItemChanged(QListWidgetItem* item);
-    void onTaskListContextMenuRequested(const QPoint& position);
+    void onTaskItemChanged(QListWidgetItem *item);
+    void onTaskListContextMenuRequested(const QPoint &position);
+    void onScheduleItemDoubleClicked(QListWidgetItem *item);
 
 private:
     Ui::MainWindow *ui;
@@ -39,7 +59,25 @@ private:
     Ui::LoginScreen *loginUi;
     Ui::SignUpScreen *signupUi;
     QStackedWidget *screenStack;
-    Database& database;
+    QWidget *calendarPage = nullptr;
+    QWidget *schedulePage = nullptr;
+    QWidget *remindersPage = nullptr;
+    QWidget *tasksPage = nullptr;
+    QWidget *weatherPage = nullptr;
+    QWidget *analysisPage = nullptr;
+    QWidget *settingsPage = nullptr;
+    QListWidget *calendarList = nullptr;
+    QListWidget *fullScheduleList = nullptr;
+    QListWidget *remindersList = nullptr;
+    QListWidget *tasksPageList = nullptr;
+    QLabel *weatherPageLabel = nullptr;
+    QLabel *analysisLabel = nullptr;
+    QLineEdit *settingsEmailEdit = nullptr;
+    QLineEdit *settingsHomeEdit = nullptr;
+    QLineEdit *settingsWorkEdit = nullptr;
+    QNetworkAccessManager *weatherManager = nullptr;
+    QTimer *reminderTimer = nullptr;
+    Database &database;
     userInfo currentUser;
 
     void loadCurrentUser();
@@ -49,9 +87,30 @@ private:
     void updateGreeting();
     void loadScheduleDisplay();
     void loadCalendarDisplay();
-    void addTaskToList(const QString& text, bool done = false);
-    void onTaskDoubleClicked(QListWidgetItem* item);
+    void addTaskToList(const QString &text, bool done = false);
+    void onTaskDoubleClicked(QListWidgetItem *item);
     void onAddScheduleItemClicked();
-    void onScheduleItemDoubleClicked(QListWidgetItem* item);
     void onAddEventClicked();
+    void showCalendarPage();
+    void showSchedulePage();
+    void showRemindersPage();
+    void showDashboardPage();
+    void showTasksPage();
+    void showWeatherPage();
+    void showAnalysisPage();
+    void showSettingsPage();
+    void importCalendar();
+    void addReminder();
+    void loadCalendarPage();
+    void loadFullSchedulePage();
+    void loadRemindersPage();
+    void requestWeather(const QString &city, bool home);
+    void checkReminders();
+    void onCalendarItemDoubleClicked(QListWidgetItem *item);
+    void exportData();
+    void saveSettings();
+    void detectLocation();
+    void loadTasksPage();
+    void buildDailyAnalysis();
+    QString weatherRecommendation(const WeatherData &data) const;
 };
