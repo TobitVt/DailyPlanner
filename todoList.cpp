d93b7b7d6a07c34e13fc06f20e334b2bd24072a7
Tobit Vervat <tobit.vervat@gmail.com>
2026-09-03 21:30:18 +0200

    Implemented task description editing

--- todoList.cpp
+++ todoList.cpp
@@ -6,7 +6,8 @@
 
 TodoList::TodoList() {}
 
-int TodoList::addTask(const QString& description, TaskPriority priority, const QDateTime& dueDate) {
+int TodoList::addTask(const QString &description, TaskPriority priority, const QDateTime &dueDate)
+{
     Task task;
     task.description = description;
     task.priority = priority;
@@ -15,46 +16,69 @@ int TodoList::addTask(const QString& description, TaskPriority priority, const Q
     return m_tasks.size() - 1;
 }
 
-void TodoList::removeTask(int index) {
-    if (index >= 0 && index < m_tasks.size()) {
+void TodoList::removeTask(int index)
+{
+    if (index >= 0 && index < m_tasks.size())
+    {
         m_tasks.remove(index);
     }
 }
 
-void TodoList::setDone(int index, bool done) {
-    if (index >= 0 && index < m_tasks.size()) {
+void TodoList::setDone(int index, bool done)
+{
+    if (index >= 0 && index < m_tasks.size())
+    {
         m_tasks[index].done = done;
     }
 }
 
-void TodoList::setPriority(int index, TaskPriority priority) {
-    if (index >= 0 && index < m_tasks.size()) {
+void TodoList::setPriority(int index, TaskPriority priority)
+{
+    if (index >= 0 && index < m_tasks.size())
+    {
         m_tasks[index].priority = priority;
     }
 }
 
-void TodoList::setDueDate(int index, const QDateTime& dueDate) {
-    if (index >= 0 && index < m_tasks.size()) {
+void TodoList::setDescription(int index, const QString &description)
+{
+    if (index >= 0 && index < m_tasks.size())
+    {
+        m_tasks[index].description = description;
+    }
+}
+
+void TodoList::setDueDate(int index, const QDateTime &dueDate)
+{
+    if (index >= 0 && index < m_tasks.size())
+    {
         m_tasks[index].dueDate = dueDate;
     }
 }
 
-const QVector<Task>& TodoList::tasks() const {
+const QVector<Task> &TodoList::tasks() const
+{
     return m_tasks;
 }
 
-QVector<Task> TodoList::incompleteTasks() const {
+QVector<Task> TodoList::incompleteTasks() const
+{
     QVector<Task> result;
-    for (const auto& t : m_tasks) {
-        if (!t.done) result.push_back(t);
+    for (const auto &t : m_tasks)
+    {
+        if (!t.done)
+            result.push_back(t);
     }
     return result;
 }
 
-QVector<Task> TodoList::completedTasks() const {
+QVector<Task> TodoList::completedTasks() const
+{
     QVector<Task> result;
-    for (const auto& t : m_tasks) {
-        if (t.done) result.push_back(t);
+    for (const auto &t : m_tasks)
+    {
+        if (t.done)
+            result.push_back(t);
     }
     return result;
 }
@@ -63,9 +87,11 @@ bool TodoList::isEmpty() const { return m_tasks.isEmpty(); }
 int TodoList::count() const { return m_tasks.size(); }
 void TodoList::clear() { m_tasks.clear(); }
 
-QString TodoList::toJson() const {
+QString TodoList::toJson() const
+{
     QJsonArray arr;
-    for (const auto& task : m_tasks) {
+    for (const auto &task : m_tasks)
+    {
         QJsonObject obj;
         obj["description"] = task.description;
         obj["done"] = task.done;
@@ -76,18 +102,21 @@ QString TodoList::toJson() const {
     return QJsonDocument(arr).toJson(QJsonDocument::Compact);
 }
 
-TodoList TodoList::fromJson(const QString& json) {
+TodoList TodoList::fromJson(const QString &json)
+{
     TodoList list;
     QJsonArray arr = QJsonDocument::fromJson(json.toUtf8()).array();
 
-    for (const auto& v : arr) {
+    for (const auto &v : arr)
+    {
         QJsonObject obj = v.toObject();
         Task task;
         task.description = obj.value("description").toString();
         task.done = obj.value("done").toBool();
         task.priority = static_cast<TaskPriority>(obj.value("priority").toInt());
         QString dueStr = obj.value("dueDate").toString();
-        if (!dueStr.isEmpty()) {
+        if (!dueStr.isEmpty())
+        {
             task.dueDate = QDateTime::fromString(dueStr, Qt::ISODate);
         }
         list.m_tasks.push_back(task);
