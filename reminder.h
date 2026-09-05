c59ea19dc061c78814ae556a85ff8ced0d6beb70
Tobit Vervat <tobit.vervat@gmail.com>
2026-09-05 13:36:45 +0200

    Added recurring reminders and snooze fields

--- reminder.h
+++ reminder.h
@@ -8,6 +8,8 @@ struct Reminder {
     QString text;
     QDateTime due;
     bool completed = false;
+    int recurrenceDays = 0;
+    QDateTime snoozedUntil;
 };
 
 class Reminders {
@@ -15,6 +17,8 @@ public:
     void add(const Reminder& reminder);
     void remove(int index);
     void setCompleted(int index, bool completed);
+    void snooze(int index, int minutes);
+    void advanceDueReminders();
     const QVector<Reminder>& all() const;
     QVector<Reminder> upcoming(int maxCount = 5) const;
     QString toJson() const;
